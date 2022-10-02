805357cd5d580658069d351532b0a818acf54bf3
Your Name <jonddos@gmail.com>
2022-10-02 16:38:58 -0500

    declaración de constantes

---
+++ src/cursoNuevo/Declaradores_Operadores.java
@@ -0,0 +1,51 @@
+package cursoNuevo;
+
+public class Declaradores_Operadores {
+    public static void main(String[] args) {
+
+
+        int a=5; //declaracion de variable en la misma linea
+
+        /*Declaración en varias lineas*/
+        int b;
+        b=7;
+
+        //---operadores---
+
+        //suma, //divion, //multiplicación
+        int c=b+a;
+
+        //Incrementable
+
+        // Se le resta 6 al 12 = (6)
+            c-=6;
+
+        // Se le suma un adicional al 6 = (7)
+            c++;
+
+        // Se le resta una unidad al 7 = (6)
+            c--;
+
+        //Se le suma 5 unidades al 6 = (11)
+            c+=5;
+
+        //impresión de la variable contenedora de la suma
+        //System.out.print(c);
+
+        final int d = 5; // declaración de una constante.
+
+        // Cuando declaras una constante no puedes redeclararla como en el caso de las variables
+        /*d = 6;*/
+
+
+        //Un mejor ejemplo
+
+        final double apulgadas = 2.54;
+        double cm = 6;
+        double resultado = cm/apulgadas;
+
+        System.out.println("En "+cm+" cm hay "+ resultado + " pulgadas");
+
+
+    }
+}
--- src/cursoNuevo/Variables.java
+++ src/cursoNuevo/Variables.java
@@ -2,8 +2,13 @@ package cursoNuevo;
 
 public class Variables {
     public static void main(String[] args) {
-        byte edad;
-        edad = 35;
+        byte edad = 35; //declaracion de una variable en una linea
+
+        /* Imprimir en consola una variable o cosas que querramos*/
+        System.out.println(edad);
+
+        edad = 75;
+
         System.out.println(edad);
 
     }
